83d6c8f1e40e8e38938988f6f8bbddc4b98c7c74
Pedro Italo <pedroitl15@gmail.com>
2025-09-18 22:05:19 -0300

    first commit of springboot aplication hotel6estrelas

---
+++ hotel6estrelas/src/main/java/com/projeto2Fase/hotel6estrelas/Hotel6estrelasApplication.java
@@ -0,0 +1,13 @@
+package com.projeto2Fase.hotel6estrelas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hotel6estrelasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Hotel6estrelasApplication.class, args);
+	}
+
+}
---
+++ hotel6estrelas/src/test/java/com/projeto2Fase/hotel6estrelas/Hotel6estrelasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto2Fase.hotel6estrelas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hotel6estrelasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
